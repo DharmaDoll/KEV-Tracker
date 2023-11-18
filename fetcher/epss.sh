70449884097c6fed6681fbf56cd17f572863c885
0xDharmaD011 <108903644+DharmaDoll@users.noreply.github.com>
2023-11-18 15:29:50 +0900

    Update epss.sh

--- fetcher/epss.sh
+++ fetcher/epss.sh
@@ -3,4 +3,3 @@ cd `dirname $0` || exit 1
 mkdir -p ../data
 wget --no-proxy https://epss.cyentia.com/epss_scores-current.csv.gz --directory-prefix=../data
 gzip -d ../data/epss_scores-current.csv.gz
-mv epss_scores-current.csv ../data
